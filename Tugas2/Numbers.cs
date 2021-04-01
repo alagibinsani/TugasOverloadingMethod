8e9aa2266ff875c6d4029c0632046b73e835966c
alagibinsani <48465507+alagibinsani@users.noreply.github.com>
2021-04-01 22:37:47 +0700

    Add files via upload

---
+++ Tugas2/Numbers.cs
@@ -0,0 +1,25 @@
+public class Numbers
+{
+	public int FindMinumum(int number1, int number2)
+	{
+		return number1 + number2;
+		
+	}
+	public int FindMinumum(int number1, int number2), float number3)
+	{
+		return number1 + number2 + number3;
+		
+	}
+	public int FindMinumum(int number1, int number2)
+	{
+		return number1 - number2;
+		
+	}
+	public int FindMinumum(int number1, int number2, float number3)
+	{
+		return number1 + number2 - number3;
+		
+		
+	}
+	
+}
